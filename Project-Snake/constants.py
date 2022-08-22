6ae6bb8963c11e731609306750884bfeb3390b85
arque1393 <aritra1393@gmail.com>
2022-08-22 09:25:24 +0530

    Snake_game_Environment+

---
+++ Project-Snake/constants.py
@@ -0,0 +1,29 @@
+from enum import Enum
+from types import SimpleNamespace
+from collections import namedtuple
+HEIGHT = 1000
+WIDTH = 1000
+
+BLOCK_SIZE = 20
+VELOCITY = 10
+
+
+class Direction(Enum):
+    RIGHT = 1
+    LEFT = -1
+    UP = 2
+    DOWN = -2
+
+
+Colours = SimpleNamespace()
+Colours.BLUE2 = (0, 100, 255)
+Colours.BLACK = (0, 0, 0)
+Colours.WHITE = (255, 255, 255)
+Colours.RED = (200, 0, 0)
+Colours.BLUE1 = (0, 0, 255)
+
+
+Point = namedtuple('Point', 'x, y')
+# print(Colours.BLACK._value_ == (0, 0, 0))
+
+image_path = "./resource/green_bg_2.jpg"
